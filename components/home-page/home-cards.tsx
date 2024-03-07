7f1828eb0dcbb004f282bb2badce49b96517cee5
navaranks <71299080+navaranks@users.noreply.github.com>
2024-03-06 17:07:58 -0800

    Updated navbar

--- components/home-page/home-cards.tsx
+++ components/home-page/home-cards.tsx
@@ -5,13 +5,13 @@ function HomeCards() {
   return (
     <div className="flex flex-col mb-32 md:mb-0 md:flex-row">
         <Link href={'/chemistry'} className="px-5 py-4 transition-colors border border-transparent rounded-lg group">
-          <img src='https://placehold.co/500x500/red/white' className='w-64 mx-auto '/>
+          <img src='https://placehold.co/500x500/DA4453/white' className='w-64 mx-auto '/>
         </Link>
-        <Link href={'/physics'} className="px-5 py-4 transition-colors border border-transparent rounded-lg group hover:border-blue-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
-          <img src='https://placehold.co/500x500/blue/white' className='w-64 mx-auto '/>
+        <Link href={'/physics'} className="px-5 py-4 transition-colors border border-transparent rounded-lg group hover:border-blue-300 hover:bg-blue-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
+          <img src='https://placehold.co/500x500/4A89DC/white' className='w-64 mx-auto '/>
         </Link>
-        <Link href={'/about'} className="px-5 py-4 transition-colors border border-transparent rounded-lg group hover:border-blue-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
-          <img src='https://placehold.co/500x500/purple/white' className='w-64 mx-auto '/>
+        <Link href={'/about'} className="px-5 py-4 transition-colors border border-transparent rounded-lg group hover:border-purple-300 hover:bg-purple-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
+          <img src='https://placehold.co/500x500/967ADC/white' className='w-64 mx-auto '/>
         </Link>
       </div>
   )
--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -1,65 +1,81 @@
 "use client";
 
-import React from "react"
-import Link from "next/link"
-import { ChevronRight, FlaskRoundIcon, HomeIcon, MenuIcon, SatelliteIcon, UserIcon } from "lucide-react"
-import { Sheet, SheetContent, SheetTrigger } from "./sheet";
-import { Button } from "./button";
+import React from "react";
+import Link from "next/link";
+import {
+  ChevronRight,
+  FlaskRoundIcon,
+  HomeIcon,
+  MenuIcon,
+  SatelliteIcon,
+  UserIcon,
+} from "lucide-react";
+import { Drawer, DrawerContent, DrawerTrigger } from "./drawer";
+import { useMediaQuery } from "@/hooks/use-media-query";
 import { Card, CardContent, CardHeader, CardTitle } from "./card";
 
-export default function navbar ({ bgColor = "bg-white"}) {
-    const menus = [
-        {icon: <HomeIcon />, title:"Home", path: "/"},
-        {icon: <FlaskRoundIcon />, title:"Chemistry", path: "/chemistry"},
-        {icon: <SatelliteIcon />, title:"Physics", path: "/physics"},
-        {icon: <UserIcon />, title:"About Me", path: "/about"}
-    ];
-    return (
-        <nav className={`${bgColor} w-full border-b md:border-0`}>
-            <div className="items-center max-w-screen-xl px-4 mx-auto md:px-8">
-                <div className="flex items-center justify-between py-3 md:py-5">
-                    <Link href="/">
-                        <h1 className="text-3xl font-bold text-white cursor-pointer">Test</h1>
-                    </Link>
-                    <div className="items-center flex-1 md:flex md:justify-end">
-                        <ul className="items-center hidden space-x-6 md:flex">
-                            {menus.map((item, idx) => (
-                                <li key={idx} className="text-lg font-bold text-white hover:text-indigo-500">
-                                    <Link href={item.path}>{item.title}</Link>
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
-                    <div className="md:hidden">
-                        <Sheet>
-                            <SheetTrigger asChild>
-                                <Button className="outline-none focus:outline-none" variant={"ghost"}>
-                                    <MenuIcon />
-                                </Button>
-                            </SheetTrigger>
-                            <SheetContent className="rounded-xl" side={"right"}>
-                                <Card>
-                                    <CardHeader>
-                                        <CardTitle>Menu</CardTitle>
-                                    </CardHeader>
-                                    <CardContent>
-                                        <div className="flex-1 pb-3 mt-8 justify-self-center md:block md:pb-0 md:mt-0">
-                                            <ul className="items-center justify-end space-y-8 md:flex md:space-x-6 md:space-y-0">
-                                                {menus.map((item, idx) => (
-                                                    <li key={idx} className="text-black hover:text-indigo-500">
-                                                        <Link href={item.path}>{item.title}</Link>
-                                                        <ChevronRight className="end-0" />
-                                                    </li>
-                                                ))}
-                                            </ul>
-                                        </div>
-                                    </CardContent>
-                                </Card>
-                            </SheetContent>
-                        </Sheet>
-                    </div>
-                </div>
-            </div>
-        </nav>
-    )
+export default function Navbar({ bgColor = "bg-white" }) {
+  const menus = [
+    { icon: <HomeIcon />, title: "Home", path: "/" },
+    { icon: <FlaskRoundIcon />, title: "Chemistry", path: "/chemistry" },
+    { icon: <SatelliteIcon />, title: "Physics", path: "/physics" },
+    { icon: <UserIcon />, title: "About Me", path: "/about" },
+  ];
+
+  const isDesktop = useMediaQuery("(min-width:768px)");
+
+  return (
+    <nav className={`${bgColor} w-full border-b md:border-0`}>
+      <div className="max-w-screen-xl px-4 mx-auto md:px-8">
+        <div className="flex items-center justify-between py-3 md:py-5">
+          <Link href="/">
+            <h1 className="text-3xl font-bold text-white cursor-pointer">
+              Test
+            </h1>
+          </Link>
+          {isDesktop ? (
+            <ul className="items-center hidden space-x-6 md:flex">
+              {menus.map((item, idx) => (
+                <li key={idx} className="text-lg font-bold text-white hover:text-indigo-500">
+                  <Link href={item.path}>
+                    {item.title}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <Drawer direction="right">
+              <DrawerTrigger asChild>
+                <button className="outline-none focus:outline-none">
+                  <MenuIcon />
+                </button>
+              </DrawerTrigger>
+              <DrawerContent>
+                <Card>
+                  <CardHeader>
+                    <CardTitle>Menu</CardTitle>
+                  </CardHeader>
+                  <CardContent>
+                    <ul className="space-y-8">
+                      {menus.map((item, idx) => (
+                        <li key={idx} className="text-black hover:text-indigo-500">
+                          {item.icon}
+                          <Link href={item.path}>
+                            
+                              {item.title}
+                              <ChevronRight className="ml-2" />
+                            
+                          </Link>
+                        </li>
+                      ))}
+                    </ul>
+                  </CardContent>
+                </Card>
+              </DrawerContent>
+            </Drawer>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
 }
