4b169d69a403e2bc89a20eef1588c0285d4e6b58
navaranks <71299080+navaranks@users.noreply.github.com>
2024-03-09 12:43:44 -0800

    Edited Chemistry Layout

--- app/(chemistry)/layout.tsx
+++ app/(chemistry)/layout.tsx
@@ -3,9 +3,9 @@ import Navbar from '@/components/ui/navbar'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className='flex flex-col h-screen md:flex-row md:overflow-hidden'>
+    <div className='flex flex-col h-screen md:overflow-hidden'>
         <Navbar bgColor='bg-[#DA4453]'/>
-        <div className="flex-grow p-6 md:p-12">{children}</div>
+        <div className="flex-grow p-6 md:p-12 bg-[#F0F0F3]">{children}</div>
         <BottomNav />
     </div>
     
--- components/chemistry/bottomnav.tsx
+++ components/chemistry/bottomnav.tsx
@@ -10,7 +10,7 @@ import { Home, Search, Bell, Mail } from 'lucide-react';
 
 const BottomNav = () => {
   const scrollDirection = useScrollingEffect();
-  const navClass = scrollDirection === 'up' ? '' : 'opacity-25 duration-500';
+  const navClass = scrollDirection === 'up' ? '' : 'opacity-100 duration-500';
 
   const {
     isHomeActive,
@@ -21,37 +21,40 @@ const BottomNav = () => {
 
   return (
     <div
-      className={`fixed bottom-0 w-full py-4 z-10 bg-zinc-100 dark:bg-zinc-950 border-t dark:border-zinc-800 border-zinc-200 shadow-lg md:hidden ${navClass}`}
+      className={`fixed bottom-0 w-full py-3 md:py-5 z-10 bg-zinc-100 dark:bg-zinc-950 border-t dark:border-zinc-800 border-zinc-200 shadow-lg  ${navClass}`}
     >
       <div className="flex flex-row items-center justify-around w-full bg-transparent">
         <Link href="/chem-home" className="relative flex flex-col items-center justify-center">
           {isHomeActive ? (
-            <Home strokeWidth={3} size={32} />
+            <Home color="#DA4453" strokeWidth={3} size={32} />
           ) : (
-            <Home strokeWidth={1} size={32} />
+            <Home size={32} />
           )}
           <span>Home</span>
         </Link>
-        <Link href="/chem-info" className="flex items-center">
+        <Link href="/chem-info" className="relative flex flex-col items-center justify-center">
           {isExploreActive ? (
-            <Search strokeWidth={3} size={32} />
+            <Search color="#DA4453" strokeWidth={3} size={32} />
           ) : (
             <Search size={32} />
           )}
+          <span>Info</span>
         </Link>
-        <Link href="/chem-schedule" className="flex items-center">
+        <Link href="/chem-schedule" className="relative flex flex-col items-center justify-center">
           {isNotificationsActive ? (
-            <Bell strokeWidth={3} size={32} />
+            <Bell color="#DA4453" strokeWidth={3} size={32} />
           ) : (
             <Bell size={32} />
           )}
+          <span>Schedule</span>
         </Link>
-        <Link href="/chem-handouts" className="flex items-center">
+        <Link href="/chem-handouts" className="relative flex flex-col items-center justify-center">
           {isMessagesActive ? (
-            <Mail strokeWidth={3} size={32} />
+            <Mail color="#DA4453" strokeWidth={3} size={32} />
           ) : (
             <Mail size={32} />
           )}
+          <span>Handouts</span>
         </Link>
       </div>
     </div>
