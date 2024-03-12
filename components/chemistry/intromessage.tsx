a62e80292bab98e5f8f62450e7c208967c9d444b
Chris <christophernavarrete00@gmail.com>
2024-03-11 21:43:55 -0700

    added cintro

--- components/chemistry/intromessage.tsx
+++ components/chemistry/intromessage.tsx
@@ -7,10 +7,11 @@ function IntroMessage() {
         <Card className='max-w-screen-xl mx-auto'>
             <CardContent className='flex flex-col md:flex-row px-4 py-3'>
                 <div className='mx-auto p-2'>
-                    <img src="https://placehold.co/500x500/DA4453/white" alt="" />
+                    <img src="/cintro.jpg" className=' max-w-xs' alt="demo" />
                 </div>
-                <div className='mx-auto p-2'>
-                    <img src="https://placehold.co/500x500/DA4453/white" alt="" />
+                <div className='w-full m-auto px-5'>
+                    <h1 className='text-3xl font-bold text-center py-2'> WELCOME CLASS OF 2023-2024</h1>
+                    <p className='text-pretty '>Welcome Scientists to our Chemistry Class Web Page! You are going to be using this page alot so make sure to bookmark it. I am very excited to have all of you in my classroom as we embark on this journey together called Chemistry. We are all going to work together, learn together, and grow together. I hope that you are equally as excited to learn about chemistry as I am to teach it. Use the tabs below to view our schedule, policies and handouts.</p>
                 </div>
             </CardContent>
         </Card>
