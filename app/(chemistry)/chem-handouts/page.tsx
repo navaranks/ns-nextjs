efbbaca3aa70338ec1eae7eb124fd76c2d7cd310
Chris <christophernavarrete00@gmail.com>
2024-03-23 17:00:07 -0700

    Added Hanouts component

--- app/(chemistry)/chem-handouts/page.tsx
+++ app/(chemistry)/chem-handouts/page.tsx
@@ -1,7 +1,47 @@
-import React from 'react'
+"use client";
 
-export default function chemhandouts() {
+import React from 'react';
+import HandoutTabs from '@/components/chemistry/handout-tabs';
+import { Card, CardContent, CardHeader, CardTitle, CardDescription } from '@/components/ui/card';
+
+const ChemHandouts = () => {
+  const tabsData = [
+    { id: 0, title: 'Unit 1', subtitle:'Metrics', content: 'Unit 1 content 2' },
+    { id: 1, title: 'Unit 2', subtitle:'Matter and Nomenclature', content: 'Unit 2 content' },
+    { id: 2, title: 'Unit 3', subtitle:'Chemical Reactions', content: 'Unit 3 content' },
+    { id: 3, title: 'Unit 4', subtitle:'Stoichiometry and The Mole', content: 'Unit 4 content' },
+    { id: 4, title: 'Unit 5', subtitle:'History of Atomic Structure', content: 'Unit 5 content' },
+    { id: 5, title: 'Unit 6', subtitle:'The Periodic Table', content: 'Unit 6 content' },
+    { id: 6, title: 'Unit 7', subtitle:'Bonding and Lewis Structures', content: 'Unit 7 content' },
+    { id: 7, title: 'Unit 8', subtitle:'Gases', content: 'Unit 8 content' },
+    { id: 8, title: 'Unit 9', subtitle:'Kinetics', content: 'Unit 9 content' },
+    { id: 9, title: 'Unit 10', subtitle:'Equilibrium', content: 'Unit 10 content' },
+    { id: 10, title: 'Unit 11', subtitle:'Acids and Bases', content: 'Unit 11 content' },
+    { id: 11, title: 'Unit 12', subtitle:'Thermodynamics', content: 'Unit 12 content' },
+    { id: 12, title: 'Unit 13', subtitle:'Electrochemistry', content: 'Unit 13 content' },
+    { id: 13, title: 'Unit 14', subtitle:'Organic Chemistry', content: 'Unit 14 content' },
+    { id: 14, title: 'Unit 15', subtitle:'Nuclear Chemistry', content: 'Unit 15 content' },
+  ];
+
+  const borderColor = 'border-chem';
+
   return (
-    <div>chem handouts</div>
-  )
-}
+    <main className="flex flex-col px-4 gap-y-4">
+      <Card className="max-w-screen-xl w-full mx-auto">
+        <CardHeader className='text-center'>
+          <CardTitle className='text-2xl font-extrabold'>These are the handouts we will be using throughout the course!</CardTitle>
+          <CardDescription><strong>Note:</strong>{" "}Check this page before class and add the necessary materials to your Chemistry Binder.</CardDescription>
+        </CardHeader>
+      </Card>
+      <Card className="max-w-screen-xl w-full mx-auto">
+        <CardContent className='p-4'>
+          <HandoutTabs tabsData={tabsData} borderColor={borderColor} />
+        </CardContent>
+      </Card>
+    </main>
+  );
+};
+
+export default ChemHandouts;
+
+
---
+++ components/chemistry/handout-tabs.tsx
@@ -0,0 +1,77 @@
+import React, { useEffect, useState, useRef } from "react";
+import { Select, SelectTrigger, SelectValue, SelectContent, SelectItem, SelectGroup, SelectLabel } from "../ui/select";
+
+interface TabData {
+    id: number;
+    title: string;
+    subtitle: string;
+    content: string;
+}
+
+interface HandoutTabsProps {
+    tabsData: TabData[];
+    borderColor: string;
+}
+const HandoutTabs: React.FC<HandoutTabsProps> = ({ tabsData, borderColor }) => {
+   
+    const [value, setValue] = useState<number>(0);
+    const tabsRef = useRef<HTMLDivElement>(null);
+    const indicatorRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        if (tabsRef.current && indicatorRef.current) {
+            const tabs= tabsRef.current.children;
+            const activeTab = tabs[value + 1] as HTMLElement;
+            indicatorRef.current.style.top= `${activeTab.offsetTop }px`;
+            indicatorRef.current.style.height= `${activeTab.clientHeight}px`;
+    }
+
+    }, [value]);
+
+    const handleChange = (index: number) => {
+        setValue(index);
+        
+    };
+
+    return (
+        <div className=" flex flex-col md:flex-row">
+            {/* Desktop Tabs */}
+            <div ref={tabsRef} className="relative hidden md:flex flex-col border-r border-gray-200 ">
+                <div ref={indicatorRef} className={`absolute right-0 border-r-4 ${borderColor} transition-all duration-300`}></div>
+                {tabsData.map((tab) => (
+                    <button
+                        key={tab.id}
+                        className={`px-4 py-5 hover:bg-chem/20 hover:rounded-lg  ${value === tab.id ? 'font-extrabold ' : ''}`}
+                        onClick={() => handleChange(tab.id)}
+                    >
+                        {tab.title}
+                    </button>
+                ))}
+            </div>
+
+            {/* Mobile Selector */}
+            <Select value={tabsData[value].id.toString()} onValueChange={(val: string) => handleChange(parseInt(val))}>
+                <SelectTrigger className="md:hidden">
+                    <SelectValue>{tabsData[value].title + ' - ' + tabsData[value].subtitle}</SelectValue>
+                </SelectTrigger>
+                <SelectContent>
+                    <SelectGroup>
+                        <SelectLabel>Handouts for</SelectLabel>
+                    {tabsData.map((tab) => (
+                        <SelectItem key={tab.id} value={tab.id.toString()}>
+                            {tab.title + ' - ' + tab.subtitle} 
+                        </SelectItem>
+                    ))}
+                    </SelectGroup>
+                </SelectContent>
+            </Select>
+
+            {/* Content */}
+            <div className="flex-grow py-4 md:p-4">
+                {tabsData[value].content}
+            </div>
+        </div>
+    );
+};
+
+export default HandoutTabs;
