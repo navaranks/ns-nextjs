f7a108751acdf9b7b8681693154c6bcc48737d93
Chris <christophernavarrete00@gmail.com>
2024-04-08 20:14:33 -0700

    added files

--- components/chemistry/handout-tabs.tsx
+++ components/chemistry/handout-tabs.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState, useRef } from "react";
 import { Select, SelectTrigger, SelectValue, SelectContent, SelectItem, SelectGroup, SelectLabel } from "../ui/select";
+import { ScrollArea } from "../ui/scroll-area";
 
 
 interface TabData {
@@ -37,12 +38,13 @@ const HandoutTabs: React.FC<HandoutTabsProps> = ({ tabsData, borderColor }) => {
     return (
         <div className=" flex flex-col md:flex-row">
             {/* Desktop Tabs */}
+            
             <div ref={tabsRef} className="relative hidden md:flex flex-col border-r border-gray-200 ">
                 <div ref={indicatorRef} className={`absolute right-0 border-r-4 ${borderColor} transition-all duration-300`}></div>
                 {tabsData.map((tab) => (
                     <button
                         key={tab.id}
-                        className={`px-4 py-5 w-24 flex-grow justify-center hover:rounded-lg hover:bg-red-50 hover:via-red-900 hover:to-red-800 active:shadow-[-1px_0px_1px_0px_#b7323f_inset,1px_0px_1px_0px_#b7323f_inset,0px_0.125rem_1px_0px_#c73a4d_inset]  ${value === tab.id ? 'font-extrabold ' : ''}`}
+                        className={`px-4 py-5 w-24 h-20 flex-grow justify-center hover:rounded-lg hover:bg-red-50 hover:via-red-900 hover:to-red-800 active:shadow-[-1px_0px_1px_0px_#b7323f_inset,1px_0px_1px_0px_#b7323f_inset,0px_0.125rem_1px_0px_#c73a4d_inset]  ${value === tab.id ? 'font-extrabold ' : ''}`}
                         onClick={() => handleChange(tab.id)}
                     >
                         {tab.title}
@@ -50,6 +52,7 @@ const HandoutTabs: React.FC<HandoutTabsProps> = ({ tabsData, borderColor }) => {
                     
                 ))}
             </div>
+            
            
 
             {/* Mobile Selector */}
@@ -71,7 +74,9 @@ const HandoutTabs: React.FC<HandoutTabsProps> = ({ tabsData, borderColor }) => {
 
             {/* Content */}
             <div className="flex-grow py-4 md:p-4">
+                <ScrollArea className="h-full">
                 {tabsData[value].content}
+                </ScrollArea>
             </div>
         </div>
     );
